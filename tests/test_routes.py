9a76b0cd1aedb2e0a8962447f6ad5947c1615c85
David Kanekanian <dkanekanian1@gmail.com>
2021-06-20 17:53:57 +0100

    test post user validation

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -13,3 +13,81 @@ def test_get_api_user(client: FlaskClient):
     assert res.is_json
     assert len(res.json) == 1
     assert type(res.json['users']) == list
+
+
+def test_post_api_user_valid(client: FlaskClient):
+    res: Response = client.post('/api/user', json={
+        "firstName": "John",
+        "lastName": "Smith",
+        "email": "johnsmith@gmail.com",
+        "phoneNum": "01234567890"
+    })
+    assert res.status_code == 200
+
+
+def test_post_api_user_missing_field(client: FlaskClient):
+    res: Response = client.post('/api/user', json={
+        "lastName": "Smith",
+        "phoneNum": "01234567890"
+    })
+    assert res.status_code == 400
+
+
+def test_post_api_user_wrong_type(client: FlaskClient):
+    res: Response = client.post('/api/user', json={
+        "firstName": "John",
+        "lastName": 23,
+        "email": "johnsmith@gmail.com",
+        "phoneNum": "01234567890"
+    })
+    assert res.status_code == 400
+
+
+def test_post_api_user_first_name_empty(client: FlaskClient):
+    res: Response = client.post('/api/user', json={
+        "firstName": "",
+        "lastName": "Smith",
+        "email": "johnsmith@gmail.com",
+        "phoneNum": "01234567890"
+    })
+    assert res.status_code == 400
+
+
+def test_post_api_user_last_name_empty(client: FlaskClient):
+    res: Response = client.post('/api/user', json={
+        "firstName": "John",
+        "lastName": "",
+        "email": "johnsmith@gmail.com",
+        "phoneNum": "01234567890"
+    })
+    assert res.status_code == 400
+
+
+def test_post_api_user_email_wrong_pattern(client: FlaskClient):
+    res: Response = client.post('/api/user', json={
+        "firstName": "John",
+        "lastName": "Smith",
+        "email": "johnsmithgmail.com",
+        "phoneNum": "01234567890"
+    })
+    assert res.status_code == 400
+
+
+def test_post_api_user_phone_too_long(client: FlaskClient):
+    res: Response = client.post('/api/user', json={
+        "firstName": "John",
+        "lastName": "Smith",
+        "email": "johnsmith@gmail.com",
+        "phoneNum": "012345678901234"
+    })
+    assert res.status_code == 400
+
+
+def test_post_api_user_phone_too_short(client: FlaskClient):
+    res: Response = client.post('/api/user', json={
+        "firstName": "John",
+        "lastName": "Smith",
+        "email": "johnsmith@gmail.com",
+        "phoneNum": "012345"
+    })
+    assert res.status_code == 400

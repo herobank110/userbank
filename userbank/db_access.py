136175613f86a7b2830c89962ac7a129fd4a1e3a
David Kanekanian <dkanekanian1@gmail.com>
2021-06-20 20:15:22 +0100

    Revert "try yield cursor from thing"
    This reverts commit 8ff973a799ea80b2a169e83a4f997b385b17d4d6.

--- userbank/db_access.py
+++ userbank/db_access.py
@@ -1,43 +1,32 @@
-from typing import List, Union
+from typing import Union
 from userbank import db_connection
 from userbank.model import PostUser
 
 
-def _query(query: str, *args: Union[str, int, float]):
+def _query(format: str, *args: Union[str, int, float]):
     """Internal implementation for database queries.
 
-    :param query: Format string, may contain %s, %d etc.
+    :param format: Format string, may contain %s, %d etc.
     :param args: Values for placeholders in `format`, if any.
-    :return: Cursor object for further manipulation, or None.
+    :return: Whether the query was successful.
     """
     try:
         conn = db_connection()
-        cur = conn.cursor()
-        cur.execute(query, args)
-        yield cur
-        conn.commit()
+        with conn, conn.cursor() as cur:
+            cur.execute(format, args)
+            yield cur
     except:
-        return
+        return False
+    return True
 
 
-def get_all_user_ids() -> Union[List[int], None]:
-    """:returns: List of ID of all users in database.
-    """
-    cur, = _query("Select id from users;")
-    rows = cur.fetchall()
-    cur.close()
-    return [r[0] for r in rows]
-
-
-def add_user(user: PostUser) -> bool:
+def add_user(user: PostUser):
     """Create a new user record.
 
     :param user: Data of user to create. ID is automatically generated.
     :return: Whether the insertion was made.
     """
-    cur, = _query("Insert ito users (first_name, last_name, email, phone_num) "
-                  "values (%s, %s, %s, %s);",
-                  user.first_name, user.last_name, user.email, user.phone_num)
-    if cur:
-        cur.close()
-    return cur is not None
+    cur = _query("Insert into users (first_name, last_name, email, phone_num) "
+                 "values (%s, %s, %s, %s);",
+                 user.first_name, user.last_name, user.email, user.phone_num)
+    return next(cur, False)
--- userbank/routes.py
+++ userbank/routes.py
@@ -1,4 +1,4 @@
-from userbank.db_access import add_user, get_all_user_ids
+from userbank.db_access import add_user
 from flask import Response, jsonify, request
 from userbank import app, db_connection
 from userbank.validation import make_post_user
@@ -6,9 +6,14 @@ from userbank.validation import make_post_user
 
 @app.route('/api/user')
 def get_api_user():
-    if not (user_ids := get_all_user_ids()):
+    try:
+        conn = db_connection()
+        with conn.cursor() as cur:
+            cur.execute("Select id from users;")
+            rows = cur.fetchall()
+    except:
         return Response(None, 500)
-    res: Response = jsonify(users=user_ids)
+    res: Response = jsonify(users=[r[0] for r in rows])
     res.status_code = 200
     return res
 

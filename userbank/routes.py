88dd8504086f1855e0974725e28b5009cb46d382
David Kanekanian <dkanekanian1@gmail.com>
2021-06-20 12:37:57 +0100

    print example row id from database

--- userbank/routes.py
+++ userbank/routes.py
@@ -1,16 +1,25 @@
 from flask import Response, jsonify, request
+from psycopg2.extras import DictCursor
 from userbank import app
+from userbank.db_connection import db_connection
 
 
 @app.route('/hi')
 def get_hi():
-    res: Response = jsonify(a=3)
+
+    print('hi')
+    conn = db_connection()
+    with conn.cursor() as cur:
+        cur.execute("select * from users;")
+        rows = cur.fetchone()
+    res: Response = jsonify(id=rows[0])
     res.status_code = 200
     return res
 
 
 @app.route('/hi/<id>')
 def get_hi_id(id):
+    print('hi')
     res: Response = jsonify(a=3, echo=id)
     res.status_code = 200
     return res

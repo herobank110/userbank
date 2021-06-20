41c99d3b244b1987477032b1c46e421fce88d165
David Kanekanian <dkanekanian1@gmail.com>
2021-06-20 18:00:51 +0100

    use default string values for model structure

--- userbank/model.py
+++ userbank/model.py
@@ -1,8 +1,9 @@
 from userbank.utils.struct import ctor
 
+
 @ctor.init_list
 class PostUser:
-    first_name: str
-    last_name: str
-    email: str
-    phone_num: str
+    first_name = ""
+    last_name = ""
+    email = ""
+    phone_num = ""

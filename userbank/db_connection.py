44bd813af1179ce90b5d917aca9fe1a35e7703b7
David Kanekanian <dkanekanian1@gmail.com>
2021-06-20 23:02:01 +0100

    format db connect function

--- userbank/db_connection.py
+++ userbank/db_connection.py
@@ -4,9 +4,8 @@ import psycopg2
 def db_connection():
     if 'conn' not in globals():
         global conn
-        conn = psycopg2.connect(
-            user="docker",
-            password="hahaha123",
-            database="Users",
-            host="172.10.10.2")
+        conn = psycopg2.connect(user="docker",
+                                password="hahaha123",
+                                database="Users",
+                                host="172.10.10.2")
     return conn

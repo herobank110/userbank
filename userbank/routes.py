8ff973a799ea80b2a169e83a4f997b385b17d4d6
David Kanekanian <dkanekanian1@gmail.com>
2021-06-20 20:14:51 +0100

    try yield cursor from thing

--- userbank/routes.py
+++ userbank/routes.py
@@ -1,4 +1,4 @@
-from userbank.db_access import add_user
+from userbank.db_access import add_user, get_all_user_ids
 from flask import Response, jsonify, request
 from userbank import app, db_connection
 from userbank.validation import make_post_user
@@ -6,14 +6,9 @@ from userbank.validation import make_post_user
 
 @app.route('/api/user')
 def get_api_user():
-    try:
-        conn = db_connection()
-        with conn.cursor() as cur:
-            cur.execute("Select id from users;")
-            rows = cur.fetchall()
-    except:
+    if not (user_ids := get_all_user_ids()):
         return Response(None, 500)
-    res: Response = jsonify(users=[r[0] for r in rows])
+    res: Response = jsonify(users=user_ids)
     res.status_code = 200
     return res
 
